3db7687531cf2c5ffa607d0cea8ba3b549376d16
lingutla <lingutlaravindra7@gmail.com>
2023-12-10 11:48:31 +0530

    added flask and aws coonfiguration

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import streamlit as st
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
 import pickle
 import string
 from nltk.corpus import stopwords
@@ -8,6 +10,9 @@ nltk.download('punkt')
 nltk.download('stopwords')
 ps = PorterStemmer()
 
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+app = Flask(__name__)
 
 def transform_text(text):
     text = text.lower()
@@ -33,23 +38,26 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_area("Enter the message")
+@app.route('/',methods=["GET",'POST']) #by default methods = ['GET']
+def new():
+    if request.method=="POST":
 
-if st.button('Predict'):
+        input_sms = request.form['text']
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+           prediction = "Spam"
+        else:
+           prediction = "Not Spam"
+        return render_template('index.html', prediction = prediction)
 
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    return render_template("index.html")
+
+app.run()
